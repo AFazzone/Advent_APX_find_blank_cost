63a1daf26f7cc8fff1e3f5cf5898a46a2107d7b6
Alicia Fazzone <31017349+AFazzone@users.noreply.github.com>
2020-09-08 11:37:58 -0400

    Add files via upload

---
+++ Advent_APX_find_blank_cost.sql
@@ -0,0 +1,14 @@
+SELECT  t.Quantity, t.TradeDate, t.SecTypeCode1, s.Symbol, b.PortfolioManager, b.Custodian, b.AccountNumber
+FROM dbo.AdvPortfolioTransaction T 
+Inner Join dbo.AdvSecurity  s
+	ON	s.SecurityID = t.SecurityID1
+INNER JOIN dbo.AdvPortfolioBase p
+       ON P.PortfolioBaseID = T.PortfolioID    
+Inner Join dbo.AdvPortfolioBaseExt b
+		ON p.PortfolioBaseID = b.PortfolioBaseID
+WHERE TransactionCode IN ('li','ti')
+AND T.SecTypeCode1 != 'ca'
+And t.Quantity IS NOT NULL
+AND (T.OriginalCost IS NULL )
+AND DATEDIFF(MONTH,T.TradeDate,GETDATE()) = 0 
+ORDER BY b.AccountNumber
